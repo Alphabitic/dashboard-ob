408685b54ed00d24f652691ad02ab9007840fb3c
Alphabitic <alphabitic@gmail.com>
2023-02-27 13:39:07 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -12,19 +12,33 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+// Middleware pour afficher les requêtes reçues
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request at ${req.url}`);
+    next();
+  });
+
 app.get("/", (req, res) => {
     res.send({ message: "Hello World!" });
 });
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/properties", propertyRouter);
-// Adding OpenAI API
+
+// Middleware pour gérer les erreurs
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Internal Server Error");
+  });
+
+ // Configuration et initialisation de l'API OpenAI
 const openaiConfig = new Configuration({
-    organizationId:'org-kDJbLtt6EZiRr2eNhugUibag',
-   apiKey: process.env.OPENAI_API_KEY,
+    organizationId: "org-kDJbLtt6EZiRr2eNhugUibag",
+    apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(openaiConfig);
-  app.post("/chatbot", async (req, res) => {
+  
+  app.post("/chatbot", async (req, res, next) => {
     const { message } = req.body;
   
     try {
@@ -42,21 +56,19 @@ const openaiConfig = new Configuration({
       res.json({ botResponse });
     } catch (error) {
       console.error(error);
-      res.status(500).send("Internal server error");
+      next(error);
     }
   });
   
-  
-const startServer = async () => {
+  const startServer = async () => {
     try {
-        connectDB(process.env.MONGODB_URL);
-
-        app.listen(8080, () =>
-            console.log("Server started on port http://localhost:8080"),
-        );
+      await connectDB(process.env.MONGODB_URL);
+      app.listen(8080, () =>
+        console.log("Server started on port http://localhost:8080")
+      );
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-};
-
-startServer();
+  };
+  
+  startServer();
