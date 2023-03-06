d419052d00921f1b50a2b78969b4814ac8b6f2d0
Alphabitic <alphabitic@gmail.com>
2023-03-06 20:02:01 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -1043,7 +1043,7 @@ mailBody += `
 `;
 
   const message = {
-    from: 'helpdesk@neocles.com',
+    from: process.env.OUTLOOK_USERNAME,
     to: process.env.RECIPIENT_EMAIL,
     subject: `Vérification quotidienne des certificats et des domaines ce ${today} (${certsToRenew.length} certificats) `,
     html: mailBody
